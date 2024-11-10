b4d74821c33ec85c5a09a644c9da62fa302ba769
Florian Piesche <florian@yellowkeycard.net>
2024-11-10 18:36:14 +0000

    Fix Exult Studio

---
+++ apps/info.exult.exult/info.exult.exult.studio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ ! -f $XDG_CONFIG_HOME/exult-studio.cfg ]]; then
+  cp /app/share/exult/exult-studio.default.cfg $XDG_CONFIG_HOME/exult-studio.cfg
+fi
+
+# Redirect any attempts to read from /app/share/data to $XDG_DATA_HOME in exult.cfg
+sed -i "s|\$XDG_DATA_HOME|$XDG_DATA_HOME|g" $XDG_CONFIG_HOME/exult-studio.cfg
+sed -i "s|/app/share/data|$XDG_DATA_HOME|g" $XDG_CONFIG_HOME/exult-studio.cfg
+
+exult_studio -c $XDG_CONFIG_HOME/exult-studio.cfg
