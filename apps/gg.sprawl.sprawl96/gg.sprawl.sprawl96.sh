f664a6cc0010bf6419839560160ed12a2e817796
Florian Piesche <florian@yellowkeycard.net>
2025-01-03 21:42:27 +0000

    Update sprawl96

--- apps/gg.sprawl.sprawl96/gg.sprawl.sprawl96.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /app/bin
-sprawl96 +game sprawl "$@"
-
-if [[ "$?" != "0" ]]; then
-    zenity --error --width=400 --title "SPRAWL 96 exited with an error" \
-    --text "For a detailed error message, please run SPRAWL 96 from a terminal window using\n \
-    <tt><b>flatpak run $FLATPAK_ID</b></tt>." --ok-label "Quit"
-fi
