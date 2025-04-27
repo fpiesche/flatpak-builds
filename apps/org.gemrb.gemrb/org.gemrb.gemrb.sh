7a29347046956d6b2b90eea9423b959be8b4236d
Florian Piesche <florian@yellowkeycard.net>
2025-04-27 23:20:42 +0100

    Graduate GemRB

--- apps/org.gemrb.gemrb/org.gemrb.gemrb.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-GEMRB_CONFIG_FILE=${XDG_CONFIG_HOME}/gemrb.cfg
-if [[ ! -f ${GEMRB_CONFIG_FILE} ]]; then cp /app/etc/gemrb/GemRB.cfg ${GEMRB_CONFIG_FILE}; fi
-
-gemrb -c ${GEMRB_CONFIG_FILE} $@
