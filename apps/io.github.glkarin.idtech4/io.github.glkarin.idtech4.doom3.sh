44dc194f0d1a9820e52385de99b0f6b2743dfac5
Florian Piesche <florian@yellowkeycard.net>
2026-02-18 15:05:41 +0000

    Add Io.github.glkarin.idtech4 (#1278)
    * add generic glkarin idtech3 bundle
    
    * Rename glkarin package and add shortcuts and icons for all ports
    
    * Rewrite metainfo description
    
    * Improve launcher scripts
    
    * EOL existing glkarin packages
    
    * Remove d3xp shortcut and split desktop files into separate module
    
    * Add generic launcher
    
    * Simplify build: Only enable options that are off by default
    
    * Fix launcher scripts
    
    * Update exceptions

---
+++ apps/io.github.glkarin.idtech4/io.github.glkarin.idtech4.doom3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BASEDIR=$XDG_DATA_HOME/doom3
+GAMEDIR=$BASEDIR/base
+
+for gamedata_file in $GAMEDIR/pak00{0..8}.pk4; do
+    if [[ ! -f $gamedata_file ]]; then
+        zenity --error --ok-label "Quit" --width=400 \
+            --title "Failed to find Doom 3 game data" --text "
+Could not find Doom 3 game data file <tt><b>$(basename $gamedata_file)</b></tt>\n\n
+Please ensure you have copied the necessary game data files
+(at least <tt><b>pak000.pk4</b></tt> through <tt><b>pak008.pk4</b></tt>)
+from a Doom 3 installation to\n<tt><b>$GAMEDIR/</b></tt>."
+        if [[ ! -d $GAMEDIR ]]; then mkdir -p $GAMEDIR; fi
+        xdg-open $GAMEDIR
+        exit 1
+    fi
+done
+
+cd /app/bin
+./Doom3 +set fs_basepath $BASEDIR $@
---
+++ apps/io.github.glkarin.idtech4/io.github.glkarin.idtech4.quake4.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+BASEDIR=$XDG_DATA_HOME/quake4
+GAMEDIR=$BASEDIR/q4base
+
+for gamedata_file in $GAMEDIR/pak{001..012}.pk4; do
+    if [[ ! -f $gamedata_file ]]; then
+        zenity --error --ok-label "Quit" --width=400 \
+            --title "Failed to find Quake 4 game data" --text "
+Could not find Quake 4 game data file <tt><b>$(basename $gamedata_file)</b></tt>\n\n
+Please ensure you have copied the necessary game data files
+(at least <tt><b>pak001.pk4</b></tt> through <tt><b>pak012.pk4</b></tt> and
+<tt><b>pak022.pk4</b></tt> through <tt><b>pak025.pk4</b></tt>)
+to\n<tt><b>$GAMEDIR/</b></tt>."
+        if [[ ! -d $GAMEDIR ]]; then mkdir -p $GAMEDIR; fi
+        xdg-open $GAMEDIR
+        exit 1
+    fi
+done
+
+if [[ ! -f $GAMEDIR/autoexec.cfg ]]; then
+    echo 'seta sys_lang "english"' >> $GAMEDIR/autoexec.cfg
+fi
+
+cd /app/bin
+./Quake4 +set fs_basepath $BASEDIR $@
